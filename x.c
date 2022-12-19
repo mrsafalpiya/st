31ca0b3aaae28e8fddf762e849650fb341dbcb1d
Safal Piya <mrsafalpiya@gmail.com>
2022-12-19 13:56:52 +0545

    Added `scrollback` patch with a tweak.
    https://st.suckless.org/patches/scrollback/
    
    Specifically the ring buffer implementation.
    
    Tweaked to scroll back half screen.

--- x.c
+++ x.c
@@ -67,6 +67,8 @@ static void zoom(const Arg *);
 static void zoomabs(const Arg *);
 static void zoomreset(const Arg *);
 static void ttysend(const Arg *);
+void kscrollup(const Arg *);
+void kscrolldown(const Arg *);
 static void invert(const Arg *);
 
 /* config.h for applying patches and the configuration. */
