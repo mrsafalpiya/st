764ba95618f676b3ff3b47235cb5ae7a352ed15b
Safal Piya <mrsafalpiya@gmail.com>
2024-09-04 01:30:09 +0545

    tweak(font): changed to monaco

--- config.def.h
+++ config.def.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SFMono Nerd Font Mono:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Monaco Nerd Font Mono:pixelsize=22:antialias=true:autohint=true";
 static char *font2[] = {
-	"Noto Color Emoji:pixelsize=16:antialias=true:autohint=true",
+	"Noto Color Emoji:pixelsize=22:antialias=true:autohint=true",
 };
 static int borderpx = 2;
 
@@ -30,7 +30,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.3;
+static float chscale = 1.0;
 
 /*
  * word delimiter string
